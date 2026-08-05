c833cac732c2a8d53bd7916e4533b2ca8d76991b
AlirezaNesari <alirezanesari@gmail.com>
2026-08-05 21:58:31 +0330

    render contacts

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,11 +2,29 @@ export default function ContactList({ contacts }) {
   return (
     <div>
       <h3>ContactList</h3>
-      <ul>
-        {contacts.map((contact)=>(
-            <li key={contact.id}>{contact.name}</li>
-        ))}
-      </ul>
+      {contacts?.length ? (
+        <ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>
+              <p>
+                {contact.name} {contact.lastName}
+              </p>
+              <p>
+                <span>آیکون</span>
+                {contact.email}
+              </p>
+              <p>
+                <span>آیکون</span>
+                {contact.phone}
+              </p>
+              <button type="button">حذف</button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>no contacts yet !</p>
+      )}
     </div>
   );
 }
+

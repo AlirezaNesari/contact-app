ca92cde56b1055c3342d017bb6157fd68acb974a
AlirezaNesari <alirezanesari@gmail.com>
2026-08-05 20:15:29 +0330

    Add ContactList component and implement empty fields validation

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -1,21 +1,29 @@
-import React from "react";
 import { useState } from "react";
+import ContactList from "./ContactList";
 
 export default function Contacts() {
   const [contacts, setContacts] = useState([]);
+  const [alert, setAlert] = useState("");
   const [contact, setContact] = useState({
     name: "",
     lastName: "",
     email: "",
     phone: "",
   });
-  const changeHndler = (event) => {
+  const changeHandler = (event) => {
     const name = event.target.name;
     const value = event.target.value;
     setContact((contact) => ({ ...contact, [name]: value }));
   };
   const addHandler = () => {
-    setContacts((contacts) => ([...contacts], contact));
+    if (
+      !contact.name ||!contact.lastName ||!contact.email ||!contact.phone 
+    ){
+      setAlert("please enter valid data!")
+      return;
+    }
+    setAlert("")
+    setContacts((contacts) => [...contacts, contact]);
     setContact({ name: "", lastName: "", email: "", phone: "" });
   };
 
@@ -26,30 +34,32 @@ export default function Contacts() {
         placeholder="Name"
         name="name"
         value={contact.name}
-        onChange={changeHndler}
+        onChange={changeHandler}
       />
       <input
         type="text"
         placeholder="lastName"
         name="lastName"
         value={contact.lastName}
-        onChange={changeHndler}
+        onChange={changeHandler}
       />
       <input
         type="email"
         placeholder="Email"
-        name="rmail"
+        name="email"
         value={contact.email}
-        onChange={changeHndler}
+        onChange={changeHandler}
       />
       <input
         type="number"
         placeholder="phone"
         name="phone"
         value={contact.phone}
-        onChange={changeHndler}
+        onChange={changeHandler}
       />
       <button onClick={addHandler}>add Contact</button>
+      <div>{alert && <p>{alert}</p>}</div>
+      <ContactList contacts={contacts} />
     </div>
   );
 }

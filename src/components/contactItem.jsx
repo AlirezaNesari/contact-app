c28f080adced88ecf66ea95fafcd70424c6cae0b
AlirezaNesari <alirezanesari@gmail.com>
2026-08-09 19:41:18 +0330

    finaly updatte

--- src/components/contactItem.jsx
+++ src/components/contactItem.jsx
@@ -1,6 +1,3 @@
-import trashIcon from "../Icons/trash.svg";
-import phoneIcon from "../Icons/phone.svg";
-import emailIcon from "../Icons/email.svg";
 import Styles from "./ContactItem.module.css";
 
 export default function ContactItem({
@@ -14,36 +11,36 @@ export default function ContactItem({
   const { id, name, lastName, email, phone } = contact;
 
   return (
-    <li className={Styles.item}>
-      <input
-        className={Styles.checkbox}
-        type="checkbox"
-        checked={selectedContacts.includes(contact.id)}
-        onChange={() => selectHandler(contact.id)}
-      />
-      <button onClick={() => favoriteHandler(id)}>
-        {contact.favorite ? "⭐" : "☆"}
-      </button>
+    <div className={Styles.container}>
+      <li className={Styles.item}>
+        <input
+          className={Styles.checkbox}
+          type="checkbox"
+          checked={selectedContacts.includes(contact.id)}
+          onChange={() => selectHandler(contact.id)}
+        />
+        <button onClick={() => favoriteHandler(id)}>
+          {contact.favorite ? "⭐" : "☆"}
+        </button>
 
-      <p>
-        {name} {lastName}
-      </p>
+        <p>
+          {name} {lastName}
+        </p>
 
-      <p>
-        <img src={emailIcon} alt="email" width={20} />
-        {email}
-      </p>
+        <p>
+          📧
+          {email}
+        </p>
 
-      <p>
-        <img src={phoneIcon} alt="phone" width={20} />
-        {phone}
-      </p>
+        <p>
+          📞
+          {phone}
+        </p>
 
-      <button onClick={() => editHandler(contact)}>✏️</button>
+        <button onClick={() => editHandler(contact)}>✏️</button>
 
-      <button onClick={() => deleteHandler(id)}>
-        <img src={trashIcon} alt="Delete" width={22} height={22} />
-      </button>
-    </li>
+        <button onClick={() => deleteHandler(id)}>🗑️</button>
+      </li>
+    </div>
   );
 }
